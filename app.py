7c81a5f2bb538a62d67319238c519be1466c08dc
rodri <rodrigo.porto1403@gmail.com>
2024-01-23 09:01:47 -0300

    Adicionado gitignore

--- app.py
+++ app.py
@@ -7,42 +7,42 @@ app = Flask(__name__)
 
 @app.route('/api/cblol', methods=['GET'])
 def cblol():
-    matches = lol_matches(cblol_2024_1, 0)
+    matches = lol_matches(cblol["2024-1"], 0)
     return jsonify(matches)
 
 @app.route('/api/ucl', methods=['GET'])
 def ucl():
-    matches = soccer_matches(ucl_2023, 1000)
+    matches = soccer_matches(ucl["2023"], 1000)
     return jsonify(matches)
 
 @app.route('/api/la-liga', methods=['GET'])
 def la_liga():
-    matches = soccer_matches(la_liga_2023, 2000)
+    matches = soccer_matches(la_liga["2023"], 2000)
     return jsonify(matches)
 
 @app.route('/api/premier-league', methods=['GET'])
 def premier_league():
-    matches = soccer_matches(premier_league_2023, 3000)
+    matches = soccer_matches(premier_league["2023"], 3000)
     return jsonify(matches)
 
 @app.route('/api/serie-a', methods=['GET'])
 def serie_a():
-    matches = soccer_matches(serie_a_2023, 4000)
+    matches = soccer_matches(serie_a["2023"], 4000)
     return jsonify(matches)
 
 @app.route('/api/bundesliga', methods=['GET'])
 def bundesliga():
-    matches = soccer_matches(bundesliga_2023, 5000)
+    matches = soccer_matches(bundesliga["2023"], 5000)
     return jsonify(matches)
 
 @app.route('/api/ligue-1', methods=['GET'])
 def ligue_1():
-    matches = soccer_matches(ligue_1_2023, 6000)
+    matches = soccer_matches(ligue_1["2023"], 6000)
     return jsonify(matches)
 
 @app.route('/api/brasileiro-a', methods=['GET'])
 def brasileiro_a():
-    matches = soccer_matches(brasileiro_a_2023, 7000)
+    matches = soccer_matches(brasileiro_a["2023"], 7000)
     return jsonify(matches)
 
 if __name__ == '__main__':
--- leagues.py
+++ leagues.py
@@ -1,85 +1,98 @@
 from object.rule import Rule
 
 SOCCER_RULE = Rule(25,18,15,13,10)
+LOL_MD1_RULE = Rule(1,0,0,0,0);
+LOL_MD3_RULE = Rule(2,1,0,0,0);
+LOL_MD5_RULE = Rule(3,2,0,0,0);
 
-cblol_2024_1 = [
-    {
-        "url" : "https://lol.fandom.com/wiki/CBLOL/2024_Season/Split_1",
-        "rule" : Rule(1,0,0,0,0),
-        "type" : "MD1"
-    },
-    # {
-    #     "url" : "https://lol.fandom.com/wiki/CBLOL/2023_Season/Split_2_Playoffs",
-    #     "rule" : Rule(2,1,0,0,0),
-    #     "type" : "MD5"
-    # }
-]
-
-lck_2024_spring = [
-    {
-        "url" : "https://lol.fandom.com/wiki/LCK/2024_Season/Spring_Season",
-        "rule" : Rule(2,1,0,0,0),
-        "type" : "MD3"
-    },
-    # {
-    #     "url" : "https://lol.fandom.com/wiki/LCK/2023_Season/Summer_Playoffs",
-    #     "rule" : Rule(3,2,0,0,0),
-    #     "type" : "MD5"
-    # }
-]
-
-ucl_2023 = [
-    {
-        "url" : "https://api.football-data.org/v4/competitions/2001/matches",
+def football_data_stage(league_id, season):
+    stage = {
+        "url" : f'https://api.football-data.org/v4/competitions/{league_id}/matches?season={season}',
         "rule" : SOCCER_RULE,
         "type" : "PLACAR"
-    },
-]
+    }
+    return stage
 
-la_liga_2023 = [
-    {
-        "url" : "https://api.football-data.org/v4/competitions/2014/matches",
-        "rule" : SOCCER_RULE,
-        "type" : "PLACAR"
-    },
-]
 
-premier_league_2023 = [
-    {
-        "url" : "https://api.football-data.org/v4/competitions/2021/matches",
-        "rule" : SOCCER_RULE,
-        "type" : "PLACAR"
-    },
-]
+cblol = {
+    "2024-1" : [
+        {
+            "url" : "https://lol.fandom.com/wiki/CBLOL/2024_Season/Split_1",
+            "rule" : LOL_MD1_RULE,
+            "type" : "MD1"
+        },
+        # {
+        #     "url" : "https://lol.fandom.com/wiki/CBLOL/2023_Season/Split_2_Playoffs",
+        #     "rule" : LOL_PLAYOFFS_RULE,
+        #     "type" : "MD5"
+        # }
+    ]
+}
 
-serie_a_2023 = [
-    {
-        "url" : "https://api.football-data.org/v4/competitions/2019/matches",
-        "rule" : SOCCER_RULE,
-        "type" : "PLACAR"
-    },
-]
+lck = {
+    "2024-1" : [
+        {
+            "url" : "https://lol.fandom.com/wiki/LCK/2024_Season/Spring_Season",
+            "rule" : LOL_MD3_RULE,
+            "type" : "MD3"
+        },
+        # {
+        #     "url" : "https://lol.fandom.com/wiki/LCK/2023_Season/Summer_Playoffs",
+        #     "rule" : LOL_MD5_RULE,
+        #     "type" : "MD5"
+        # }
+    ]   
+}
 
-bundesliga_2023 = [
-    {
-        "url" : "https://api.football-data.org/v4/competitions/2002/matches",
-        "rule" : SOCCER_RULE,
-        "type" : "PLACAR"
-    },
-]
+ucl = {
+    "2023" : [ football_data_stage(2001, 2023) ],
+}
 
-ligue_1_2023 = [
-    {
-        "url" : "https://api.football-data.org/v4/competitions/2015/matches",
-        "rule" : SOCCER_RULE,
-        "type" : "PLACAR"
-    },
-]
+la_liga = {
+    "2023" : [ football_data_stage(2014, 2023) ],
+}
 
-brasileiro_a_2023 = [
-    {
-        "url" : "https://api.football-data.org/v4/competitions/2013/matches",
-        "rule" : SOCCER_RULE,
-        "type" : "PLACAR"
-    },
-]
+premier_league = {
+    "2023" : [ football_data_stage(2021, 2023) ],
+}
+
+serie_a = {
+    "2023" : [ football_data_stage(2019, 2023) ],
+}
+
+bundesliga = {
+    "2023" : [ football_data_stage(2002, 2023) ],
+}
+
+ligue_1 = {
+    "2023" : [ football_data_stage(2015, 2023) ],
+}
+
+brasileiro_a = {
+    "2023" : [ football_data_stage(2013, 2023) ],
+}
+
+# eredivise_2023 = [
+#     {
+#         "url" : "https://api.football-data.org/v4/competitions/2003/matches",
+#         "rule" : SOCCER_RULE,
+#         "type" : "PLACAR"
+#     },
+# ]
+
+# liga_portugal_2023 = [
+#     {
+#         "url" : "https://api.football-data.org/v4/competitions/2017/matches",
+#         "rule" : SOCCER_RULE,
+#         "type" : "PLACAR"
+#     },
+# ]
+
+# eurocopa_2024 = [
+#     {
+#         "url" : "https://api.football-data.org/v4/competitions/2018/matches",
+#         "rule" : SOCCER_RULE,
+#         "type" : "PLACAR"
+#     },
+# ]
+
