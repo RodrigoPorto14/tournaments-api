99e5f1ae1d3e7a2c18ccc6e6f857867375a17166
rodri <rodrigo.porto1403@gmail.com>
2024-02-04 11:34:06 -0300

    Adicionado CBLOL Academy

--- constants.py
+++ constants.py
@@ -23,5 +23,17 @@ CBLOL_LOGOS = {
     "Fluxo" : "https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Fluxo_escudo.png/225px-Fluxo_escudo.png",
     "Vivo Keyd Stars" : "https://api.draft5.gg/teams/1050/logo",
     "RED Canids" : "https://upload.wikimedia.org/wikipedia/pt/d/df/RED_Canids_Kalunga.png",
-    "FURIA" : "https://upload.wikimedia.org/wikipedia/pt/thumb/f/f9/Furia_Esports_logo.png/218px-Furia_Esports_logo.png"
+    "FURIA" : "https://upload.wikimedia.org/wikipedia/pt/thumb/f/f9/Furia_Esports_logo.png/218px-Furia_Esports_logo.png",
+    "paiN Gaming Academy": "https://upload.wikimedia.org/wikipedia/pt/thumb/5/5d/PainGaming.png/225px-PainGaming.png",
+    "LOUD Academy": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/LOUD_logo.svg/225px-LOUD_logo.svg.png",
+    "Liberty Academy" : "https://images.squarespace-cdn.com/content/v1/61afb10ae1f7cf52bfb3dd13/1a627bac-be78-47d3-917f-72b4d880beba/%23557q1m_presskit_logo_cyan_transparent_RGB.png",
+    "INTZ Academy" : "https://upload.wikimedia.org/wikipedia/pt/thumb/c/c3/Logo_INTZ.png/263px-Logo_INTZ.png",
+    "KaBuM! Academy" : "https://cdn.escharts.com/uploads/public/63b/d6e/6ea/63bd6e6ea6631174338548.png",
+    "Los Grandes Academy" : "https://upload.wikimedia.org/wikipedia/pt/a/af/Los_Grandes_logo.png",
+    "Fluxo Academy" : "https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Fluxo_escudo.png/225px-Fluxo_escudo.png",
+    "Vivo Keyd Stars Academy" : "https://api.draft5.gg/teams/1050/logo",
+    "RED Academy" : "https://upload.wikimedia.org/wikipedia/pt/d/df/RED_Canids_Kalunga.png",
+    "FURIA Academy" : "https://upload.wikimedia.org/wikipedia/pt/thumb/f/f9/Furia_Esports_logo.png/218px-Furia_Esports_logo.png",
+    "Flamengo Academy" : "https://static.wikia.nocookie.net/lolesports_gamepedia_en/images/3/3d/Flamengo_Esportslogo_square.png/revision/latest/scale-to-width-down/123?cb=20221028053727",
+    "Tropa XV" : "https://static.wikia.nocookie.net/lolesports_gamepedia_en/images/b/b3/Tropa_XVlogo_square.png/revision/latest/scale-to-width-down/123?cb=20240117124549"
 }
